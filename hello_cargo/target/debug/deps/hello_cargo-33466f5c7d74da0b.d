711ad33cdf4dd00154416b8bac9ca8130b12f93e
Andrew Turpanov <catchaglimpse@mail.ru>
2023-12-15 15:36:01 +0800

    added 'cargo' to the repo

---
+++ hello_cargo/target/debug/deps/hello_cargo-33466f5c7d74da0b.d
@@ -0,0 +1,5 @@
+/home/andrew/projects/labs/hello_cargo/target/debug/deps/hello_cargo-33466f5c7d74da0b: src/main.rs
+
+/home/andrew/projects/labs/hello_cargo/target/debug/deps/hello_cargo-33466f5c7d74da0b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/andrew/projects/labs/hello_cargo/target/debug/hello_cargo: /home/andrew/projects/labs/hello_cargo/src/main.rs
